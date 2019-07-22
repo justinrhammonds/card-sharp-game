3c2a51f9d28df0dbcfc07d8cbf9e466b0dd4f06b
Andrew Boyd <andrew@wearebraid.com>
2019-07-22 13:44:46 -0400

    programatically generates the deck

--- src/data/cards.js
+++ src/data/cards.js
@@ -1,59 +1,38 @@
-const cards = [
-  { suit: "hearts", name: "two", value: 2 },
-  { suit: "hearts", name: "three", value: 3 },
-  { suit: "hearts", name: "four", value: 4 },
-  { suit: "hearts", name: "five", value: 5 },
-  { suit: "hearts", name: "six", value: 6 },
-  { suit: "hearts", name: "seven", value: 7 },
-  { suit: "hearts", name: "eight", value: 8 },
-  { suit: "hearts", name: "nine", value: 9 },
-  { suit: "hearts", name: "ten", value: 10 },
-  { suit: "hearts", name: "jack", value: 11 },
-  { suit: "hearts", name: "queen", value: 12 },
-  { suit: "hearts", name: "king", value: 13 },
-  { suit: "hearts", name: "ace", value: 14 },
-  { suit: "diamonds", name: "two", value: 2 },
-  { suit: "diamonds", name: "three", value: 3 },
-  { suit: "diamonds", name: "four", value: 4 },
-  { suit: "diamonds", name: "five", value: 5 },
-  { suit: "diamonds", name: "six", value: 6 },
-  { suit: "diamonds", name: "seven", value: 7 },
-  { suit: "diamonds", name: "eight", value: 8 },
-  { suit: "diamonds", name: "nine", value: 9 },
-  { suit: "diamonds", name: "ten", value: 10 },
-  { suit: "diamonds", name: "jack", value: 11 },
-  { suit: "diamonds", name: "queen", value: 12 },
-  { suit: "diamonds", name: "king", value: 13 },
-  { suit: "diamonds", name: "ace", value: 14 },
-  { suit: "spades", name: "two", value: 2 },
-  { suit: "spades", name: "three", value: 3 },
-  { suit: "spades", name: "four", value: 4 },
-  { suit: "spades", name: "five", value: 5 },
-  { suit: "spades", name: "six", value: 6 },
-  { suit: "spades", name: "seven", value: 7 },
-  { suit: "spades", name: "eight", value: 8 },
-  { suit: "spades", name: "nine", value: 9 },
-  { suit: "spades", name: "ten", value: 10 },
-  { suit: "spades", name: "jack", value: 11 },
-  { suit: "spades", name: "queen", value: 12 },
-  { suit: "spades", name: "king", value: 13 },
-  { suit: "spades", name: "ace", value: 14 },
-  { suit: "clubs", name: "two", value: 2 },
-  { suit: "clubs", name: "three", value: 3 },
-  { suit: "clubs", name: "four", value: 4 },
-  { suit: "clubs", name: "five", value: 5 },
-  { suit: "clubs", name: "six", value: 6 },
-  { suit: "clubs", name: "seven", value: 7 },
-  { suit: "clubs", name: "eight", value: 8 },
-  { suit: "clubs", name: "nine", value: 9 },
-  { suit: "clubs", name: "ten", value: 10 },
-  { suit: "clubs", name: "jack", value: 11 },
-  { suit: "clubs", name: "queen", value: 12 },
-  { suit: "clubs", name: "king", value: 13 },
-  { suit: "clubs", name: "ace", value: 14 },
+let cards = [];
+const suits = ['hearts', 'diamonds', 'spades', 'clubs'];
+const cardNames = {
+  "two": 2,
+  "three": 3,
+  "four": 4,
+  "five": 5,
+  "six": 6,
+  "seven": 7,
+  "eight": 8,
+  "nine": 9,
+  "ten": 10,
+  "jack": 11,
+  "queen": 12,
+  "king": 13,
+  "ace": 14,
+}
+const jokers = [
   { suit: "hearts", name: "red-joker", value: 0 },
-  { suit: "spades", name: "black-joker", value: 0 },    
+  { suit: "spades", name: "black-joker", value: 0 },
 ];
+
+// generate deck
+suits.forEach((suit) => {
+  Object.keys(cardNames).forEach((name, value) => {
+    cards.push({
+      suit: suit,
+      name: name,
+      value: value
+    })
+  })
+});
+
+// append the jokers
+cards = cards.concat(jokers)
 
 export default cards;
 
