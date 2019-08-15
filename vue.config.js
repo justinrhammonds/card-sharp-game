7513ae8ae59ea0a3eafca52832fc225a60503481
Justin Hammonds <justinrhammonds@gmail.com>
2019-08-14 22:04:16 -0400

    testing public path for prod netlify

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/card-sharp-game/'
-    : '/'
-}
+  // publicPath: process.env.NODE_ENV === 'production'
+  //   ? '/card-sharp-game/'
+  //   : '/'
+};
